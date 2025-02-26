7ecfe3c82354dcbf8b5a429f39b7a311061fe9d4
nafis15510 <shihabnafis17@gmail.com>
2025-02-26 13:58:49 +0700

    Add files via upload

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl {
+  static String tambah = "http://192.168.137.191/foldernafis/create.php";
+  static String lihat = "http://192.168.137.191/foldernafis/detail.php";
+  static String edit = "http://192.168.137.191/foldernafis/update.php";
+  static String hapus = "http://192.168.137.191/foldernafis/delete.php";
+  static String data = "http://192.168.137.191/foldernafis/list.php";
+}
